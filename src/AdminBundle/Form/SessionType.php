4c0b30c69a3aa0cae24db02ef226f4819cc6fb0b
Radh <med.radhouen.elhaj@gmail.com>
2016-05-17 22:50:24 +0000

    V

---
+++ src/AdminBundle/Form/SessionType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('anne')
+            ->add('datedeb',DateType::class)
+            ->add('datefin',DateType::class)
+            ->add('etat',ChoiceType::class, array(
+        'choices'  => array(
+            '1' => '1',
+            '0' => '0',
+        )))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AdminBundle\Entity\Session'
+        ));
+    }
+}
